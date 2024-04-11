bead267d6754017372a4963235f3ec9be1ffaa8a
Joshua Kravitz <2271524@johnabbottcollege.net>
2024-04-11 09:57:57 -0400

    Created UI for categories window wand created categories interface.

---
+++ CalendarUI/CategoriesViewInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarUI
+{
+    public interface CategoriesViewInterface
+    {
+        public void AddCategory();
+
+        public void RemoveCategory();
+
+        public void SetError();
+
+        public void ClearError();
+    }
+}
