3e14fadfcdb359344d6e3f2d82090844c375df0e
Lucas Lalumière <lucas.lalum@gmail.com>
2024-04-11 09:40:06 -0400

    Set some quick ideas for possible methods to be inside our interfaces

--- CalendarUI/ViewInterface.cs
+++ CalendarUI/ViewInterface.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Calendar;
 
 namespace CalendarUI
 {
     class ViewInterface
     {
-
+        // This is the interface for the View class
+        public interface IView
+        {
+            void SetEvents(List<Event> events);
+            void SetCategories(List<Category> categories);
+            void SetEvent(Event theEvent);
+            void SetCategory(Category theCategory);
+            void SetDate(DateTime date);
+            void SetDuration(Double duration);
+            void SetDetails(String details);
+            void SetCategoryID(int categoryID);
+            void SetEventID(int eventID);
+            void SetError(String error);
+            void ClearError();
+            void ClearEvent();
+            void ClearCategory();
+            void ClearEvents();
+            void ClearCategories();
+            void ClearDate();
+            void ClearDuration();
+            void ClearDetails();
+            void ClearCategoryID();
+            void ClearEventID();
+        }
     }
 }
