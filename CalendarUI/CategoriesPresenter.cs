ef3e2dfa636bde1a8586afbecece8513baf0d20f
Joshua Kravitz <2271524@johnabbottcollege.net>
2024-04-11 10:47:34 -0400

    Made changes to presenter and window.

---
+++ CalendarUI/CategoriesPresenter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calendar;
+namespace CalendarUI
+{
+    class CategoriesPresenter
+    {
+        private readonly CategoriesViewInterface _view;
+        private readonly HomeCalendar _model;
+        public CategoriesPresenter(CategoriesViewInterface v, HomeCalendar model)
+        {
+            _view = v;
+            _model = model;
+        }
+        public void ProcessInput(string categoryName,Category.CategoryType type)
+        {
+            _model.categories.Add(categoryName, type);
+            _view.AddCategory();
+        }
+    }
+}
--- CalendarUI/CategoriesWindow.xaml.cs
+++ CalendarUI/CategoriesWindow.xaml.cs
@@ -11,17 +11,44 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using Calendar;
 namespace CalendarUI
 {
     /// <summary>
     /// Interaction logic for CategoriesWindow.xaml
     /// </summary>
-    public partial class CategoriesWindow : Window
+    public partial class CategoriesWindow : Window, CategoriesViewInterface
     {
+        private readonly CategoriesPresenter _presenter;
         public CategoriesWindow()
         {
             InitializeComponent();
+        }
+
+        public void AddCategory()
+        {
+           Sucess.Visibility = Visibility.Visible;
+        }
+
+        public void ClearError()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetError()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Btn_AddCategory(object sender, RoutedEventArgs e)
+        {
+            Sucess.Visibility = Visibility.Collapsed;
+
         }
     }
 }
