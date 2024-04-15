1099da3ccade48ea85c9af272ff3c7974304310a
Joshua Kravitz <2271524@johnabbottcollege.net>
2024-04-15 12:30:28 -0400

    Merged from get calendar name. Added method to presenter.

--- CalendarUI/Presenter.cs
+++ CalendarUI/Presenter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,11 @@ namespace CalendarUI
         public void InitlializeHomeCalendar(string databaseFilename, bool newDB)
         {
             model = new HomeCalendar(databaseFilename + ".db", newDB);
+        }
+        public void ProcessInput(string categoryName, Category.CategoryType type)
+        {
+            _model.categories.Add(categoryName, type);
+            _view.AddCategory();
         }
     }
 }
